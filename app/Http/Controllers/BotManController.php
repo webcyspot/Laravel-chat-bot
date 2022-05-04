620dcf204117e4658d7fe075d6df1fe0b7e01dda
webcyspot <spot.yc55@gmail.com>
2022-05-04 05:26:49 +0300

    laravel chat bot origin

---
+++ app/Http/Controllers/BotManController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use BotMan\BotMan\BotMan;
+use Illuminate\Http\Request;
+use App\Conversations\ExampleConversation;
+use BotMan\BotMan\Messages\Attachments\Image;
+use BotMan\BotMan\Messages\Outgoing\OutgoingMessage;
+use Illuminate\Foundation\Inspiring;
+use BotMan\BotMan\Messages\Incoming\Answer;
+use BotMan\BotMan\Messages\Outgoing\Question;
+use BotMan\BotMan\Messages\Outgoing\Actions\Button;
+use BotMan\BotMan\Messages\Conversations\Conversation;
+class BotManController extends Controller
+{
+    /**
+     * Place your BotMan logic here.
+     */
+    public function handle()
+    {
+        $botman = app('botman');
+
+        $botman->listen();
+    }
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function tinker()
+    {
+        return view('tinker');
+    }
+
+    /**
+     * Loaded through routes/botman.php
+     * @param  BotMan $bot
+     */
+    public function imageattachment(BotMan $botman)
+    {
+        $question = Question::create("Huh - you woke me up. What do you need?")
+            ->fallback('Unable to ask question')
+            ->callbackId('ask_reason')
+            ->addButtons([
+                Button::create('Tell a joke')->value('joke'),
+                Button::create('Give me a fancy quote')->value('quote'),
+            ]);
+
+        return $this->ask($question, function (Answer $answer) {
+            if ($answer->isInteractiveMessageReply()) {
+                if ($answer->getValue() === 'joke') {
+                    $joke = json_decode(file_get_contents('http://api.icndb.com/jokes/random'));
+                    $this->say($joke->value->joke);
+                } else {
+                    $this->say(Inspiring::quote());
+                }
+            }
+        });
+    }
+    public function startConversation(BotMan $bot)
+    {
+        $bot->startConversation(new ExampleConversation());
+    }
+}
